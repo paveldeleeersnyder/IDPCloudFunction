c05526968af78bb1137135440b3b18ee37f0257d
paveldel <pavel.deleersnyder@student.howest.be>
2026-02-19 14:58:07 +0100

    make final changes to succesfully call gemini

--- llm.py
+++ llm.py
@@ -1,31 +1,40 @@
 from google import genai
-from google.genai.types import GenerateContentConfig, HttpOptions
-import data
+from google.genai import types
+from data import insert_quote
 import os
 from dotenv import load_dotenv
+from typing import TypedDict, List, Optional
 
 load_dotenv()
 
 api_key = os.getenv('AI_API_KEY')
 
-class Quote:
-    def __init__(self):
-        self.a = 5
-
-insert_quote_input = {
-    "supplier": str,
-    "date": str,
-    "terms_and_conditions": str,
-    "phone_number": str,
-    "products": list[
-        {
-            "amount": int,
-            "unit": str,
-            "price": float,
-            "name": str
+tools = [types.Tool(function_declarations=[types.FunctionDeclaration(
+    name="insert_quote",
+    description="insert the technical details of a quote to the database",
+    parameters={
+          "type": "object",
+          "required": ["supplier", "date", "terms_and_conditions", "phone_number", "products"],
+          "properties": {
+            "supplier": {"type": "string", "description": "Supplier name of this quote"},
+            "date": {"type": "string", "format": "date", "description": "The date the quote was sent"},
+            "terms_and_conditions": {"type": "string", "description": "A short description of the general terms and conditions"},
+            "phone_number": {"type": "string", "description": "Phone number of the supplier"},
+            "products": {
+              "type": "array",
+              "items": {
+                "type": "object",
+                "required": ["amount", "price", "name"],
+                "properties": {
+                    "amount": {"type": "number", "description": "The amount of times the product was ordered"},
+                    "unit": {"type": "string", "description": "A short description of the unit in which the product was ordered (ex. pcs, m, ...) null when it doesn't make sense"},
+                    "price": {"type": "number", "description": "Price per unit of product ordered"},
+                    "name": {"type": "string", "description": "An identifier of the product which was ordered"},
+                }
+            }, "description": "A list of the products ordered in the quote"}
+          }
         }
-    ]
-}
+    )])]
 
 model = "gemini-2.5-flash"
 with open("quote.txt", "r") as file:
@@ -33,37 +42,34 @@ with open("quote.txt", "r") as file:
 
 def get_quote_details(quote_text):
     prompt = base_prompt + "\n" + quote_text
+    history = [types.Content(role="user", parts=[types.Part(text=prompt)])]
 
-    client = genai.Client(http_options=HttpOptions(api_version="v1"), api_key=api_key)
+    client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(
-        model=model,
-        contents=prompt,
-        config=GenerateContentConfig(
-            tools=[insert_quote_tool],
-            temperature=0,
-        ),
-    )
+    response = generate_response(client, history)
+
+    print(response)
+
+    while response.function_calls != None:
+        function_call = response.function_calls[0]
+        history.append(response.candidates[0].content)
+        call = response.function_calls[0]
+        result = insert_quote(function_call.args)
+        print(f"Tool called: {result}")
+        history.append(types.Content(role="tool", parts=[types.Part.from_function_response(name=function_call.name, response={"result": result})]))
+        response = generate_response(client, history)
+        
+    types.FunctionResponse()
 
     return response.text
 
-def insert_quote_tool(quote: {"some": insert_quote_input}) -> str:
-    """Method for inserting details of a quote into database.
+def generate_response(client, history):
+    print("sending request to llm")
 
-    Args:
-        object with supplier and product info:
-        {"supplier": str,
-        "date": str,
-        "terms_and_conditions": str,
-        "phone_number": str,
-        "products": list[
-            {
-                "amount": int,
-                "unit": str,
-                "price": float,
-                "name": str
-            }
-        ]}
-    """
-
-    return data.insert_quote(insert_quote_input)
+    return client.models.generate_content(
+        model=model,
+        contents=history,
+        config=types.GenerateContentConfig(
+            tools=tools,
+        ),
+    )
